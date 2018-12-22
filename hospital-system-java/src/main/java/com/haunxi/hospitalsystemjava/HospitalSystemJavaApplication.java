d9897089aa254b89e0203b06ced51fb83f888941
huanxi <1355473748@qq.com>
2018-12-23 03:55:37 +0800

    first commit

---
+++ hospital-system-java/src/main/java/com/haunxi/hospitalsystemjava/HospitalSystemJavaApplication.java
@@ -0,0 +1,14 @@
+package com.haunxi.hospitalsystemjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSystemJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSystemJavaApplication.class, args);
+    }
+
+}
+
